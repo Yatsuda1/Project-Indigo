c6e0d04be761a09c15f30696eb17a5ef9a42a23a
Madi <155908862+Team5505-V2Scouting@users.noreply.github.com>
2024-04-17 09:12:28 -0400

    Import Initial Files
    Imported root files from ScoutingPASS

---
+++ 2024/crescendo_config.js
@@ -0,0 +1,178 @@
+var config_data = `
+{
+  "dataFormat": "tsv",
+  "title": "Scouting PASS 2024",
+  "page_title": "Crescendo",
+  "checkboxAs": "10",
+  "prematch": [
+    { "name": "Scouter Initials",
+      "code": "s",
+      "type": "scouter",
+      "size": 5,
+      "maxSize": 5,
+      "required": "true"
+    },
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2024micmp2"
+    },
+    { "name": "Match Level",
+      "code": "l",
+      "type": "level",
+      "choices": {
+        "qm": "Quals<br>",
+        "sf": "Semifinals<br>",
+        "f": "Finals"
+      },
+      "defaultValue": "qm",
+      "required": "true"
+    },
+    { "name": "Match #",
+      "code": "m",
+      "type": "match",
+      "min": 1,
+      "max": 150,
+      "required": "true"
+    },
+    { "name": "Robot",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r1": "Red-1",
+        "b1": "Blue-1<br>",
+        "r2": "Red-2",
+        "b2": "Blue-2<br>",
+        "r3": "Red-3",
+        "b3": "Blue-3"
+      },
+      "required":"true"
+    },
+    { "name": "Team #",
+      "code": "t",
+      "type": "team",
+      "min": 1,
+      "max": 99999
+    },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png",
+      "clickRestriction": "one",
+      "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
+      "shape": "circle 5 black red true"
+    }
+  ],
+  "auton": [
+    { "name": "Auton Note Pickup",
+      "code": "anp",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png"
+    }, 
+    { "name": "Leave Starting Zone",
+      "code": "al",
+      "type": "bool"
+    },
+    { "name": "Amp Scores",
+      "code": "aas",
+      "type": "counter"
+    },
+    { "name": "Speaker Scores",
+      "code": "ass",
+      "type": "counter"
+    }
+  ],
+  "teleop": [
+    { "name": "Speaker Shot Position",
+      "code": "tsp",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png"
+    }, 
+    { "name": "Amp Scores",
+      "code": "tas",
+      "type": "counter"
+    },
+    { "name": "Speaker Scores (Normal)",
+      "code": "tss",
+      "type": "counter"
+    },
+    { "name": "Speaker Scores (Amplified)",
+      "code": "tsa",
+      "type": "counter"
+    },
+    { "name": "Missed Shots",
+      "code": "tms",
+      "type": "counter"
+    },
+    { "name": "Passing Shots",
+      "code": "tf",
+      "type": "counter"
+    }, 
+    { "name": "Pickup From",
+      "code": "tpu",
+      "type": "radio",
+      "choices": {
+        "s": "Source<br>",
+        "f": "Floor<br>",
+        "b": "Both<br>",
+        "x": "Not Attempted"
+      },
+      "defaultValue": "x"
+    }
+  ],
+  "endgame": [
+    { "name": "Stage Timer",
+      "code": "dt",
+      "type": "timer"
+    },
+    { "name": "Final Status",
+      "code": "fs",
+      "type":"radio",
+      "choices": {
+        "p": "Parked<br>",
+        "o": "Onstage<br>",
+        "h": "Harmony<br>",
+        "a": "Attempted but failed<br>",
+        "x": "Not attempted"
+      },
+      "defaultValue": "x"
+    },
+    {
+      "name": "Chain Spotlighted?",
+      "code": "spt",
+      "type": "bool"
+    },
+    { "name": "Note(s) in Trap",
+      "code": "nit",
+      "type": "counter"
+    }
+  ],
+  "postmatch": [
+    { "name": "Played Defense",
+      "code": "def",
+      "type": "bool"
+    },
+    { "name": "Feeder Bot",
+      "code": "fed",
+      "type": "bool"
+    },
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
+      "code": "tip",
+      "type": "bool"
+    },
+    { "name": "Dropped Notes (>2)",
+      "code": "dn",
+      "type": "bool"
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 150
+    }
+  ]
+}`;
---
+++ 2024/crescendo_pit_config.js
@@ -0,0 +1,71 @@
+var config_data = `
+{
+  "dataFormat": "tsv",
+  "title": "Scouting PASS 2024",
+  "page_title": "Crescendo",
+  "pitConfig": "true",
+  "prematch": [
+    { "name": "Team Number",
+      "code": "t",
+      "type": "number"
+    },
+    { "name": "Dimensions",
+      "code": "dmn",
+      "type": "text"
+    },
+    { "name": "Weight",
+      "code": "wei",
+      "type": "number",
+      "defaultValue": "0"
+    },
+    { "name": "Drivetrain",
+      "code": "drv",
+      "type": "radio",
+      "choices": {
+        "s": "Swerve<br>",
+        "t": "Tank<br>",
+        "m": "Mechanum<br>",
+        "o": "Other"
+      },
+      "defaultValue": "o"
+    },
+    { "name": "Climb",
+      "code": "clm",
+      "type": "bool"
+    },
+    { "name": "Trap Scoring",
+      "code": "trp",
+      "type": "bool"
+    },
+    { "name": "Autos",
+      "code": "aut",
+      "type": "text",
+      "size": 20,
+      "maxSize": 250
+    },
+    { "name": "Other Special Features",
+      "code": "sct",
+      "type": "text",
+      "size": 20,
+      "maxSize": 250
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 20,
+      "maxSize": 250
+    },
+    { "name": "Took a picture?",
+      "code": "pic",
+      "type": "bool"
+    }
+  ],
+  "auton": [
+  ],
+  "teleop": [
+  ],
+  "endgame": [
+  ],
+  "postmatch": [
+  ]
+}`;
