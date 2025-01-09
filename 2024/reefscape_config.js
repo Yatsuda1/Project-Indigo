cc678e703bb320174e6317570037a761d4a783bc
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2025-01-09 08:39:35 -0500

    Used Jsonlint Again

--- 2024/reefscape_config.js
+++ 2024/reefscape_config.js
@@ -1,159 +1,182 @@
-var config_data = `
 {
-  "dataFormat": "tsv",
-  "title": "Reefscape Scouting 2025",
-  "page_title": "REEFSCAPE",
-  "checkboxAs": "10",
-  "prematch": [
-    { "name": "Scouter",
-      "code": "s",
-      "type": "scouter",
-      "size": 5,
-      "maxSize": 99,
-      "required": "true"
-    },
-    { "name": "Event",
-      "code": "e",
-      "type": "event",
-      "defaultValue": "2025milac",
-      "required": "true"
-    },
-    { "name": "Match #",
-      "code": "m",
-      "type": "match",
-      "min": 1,
-      "max": 150,
-      "required": "true"
-    },
-    { "name": "Robot",
-      "code": "r",
-      "type": "robot",
-      "choices": {
-        "r1": "Red-1",
-        "b1": "Blue-1<br>",
-        "r2": "Red-2",
-        "b2": "Blue-2<br>",
-        "r3": "Red-3",
-        "b3": "Blue-3"
-      },
-      "required":"true"
-    },
-    { "name": "Team #",
-      "code": "t",
-      "type": "team",
-      "min": 1,
-      "max": 99999
-    },
-  ],
-  "auton": [
-    { "name": "Leave Starting Line",
-      "code": "al",
-      "type": "bool"
-    },
-    { "name": "Coral L1",
-      "code": "ac1",
-      "type": "counter"
-    },
-    { "name": "Coral L2",
-      "code": "ac2",
-      "type": "counter"
-    },
-    { "name": "Coral L3",
-      "code": "ac3",
-      "type": "counter"
-    },
-    { "name": "Coral L4",
-      "code": "ac4",
-      "type": "counter"
-    },
-    { "name": "Processor Score",
-      "code": "aps",
-      "type": "counter"
-    },
-    { "name": "Net Score",
-      "code": "ans",
-      "type": "counter"
-    }
-  ],
-  "teleop": [
-    { "name": "Coral L1",
-      "code": "tc1",
-      "type": "counter"
-    },
-    { "name": "Coral L2",
-      "code": "tc2",
-      "type": "counter"
-    },
-    { "name": "Coral L3",
-      "code": "tc3",
-      "type": "counter"
-    },
-    { "name": "Coral L4",
-      "code": "tc4",
-      "type": "counter"
-    },
-    { "name": "Processor Score",
-      "code": "tps",
-      "type": "counter"
-    },
-    { "name": "Net Score",
-      "code": "tns",
-      "type": "counter"
-    },
-    { "name": "Picked Up Coral From",
-      "code": "tpu",
-      "type": "radio",
-      "choices": {
-        "s": "Coral Station<br>",
-        "f": "Floor<br>",
-        "b": "Both<br>",
-        "x": "Not Attempted"
-      },
-      "defaultValue": "x"
-    },
-    { "name": "Scored in Opponent Processor",
-      "code": "opp",
-      "type": "bool"
-    }
-  ],
-  "endgame": [
-    { "name": "Barge Timer",
-      "code": "ebt",
-      "type": "timer"
-    },
-    { "name": "Final Status",
-      "code": "efs",
-      "type":"radio",
-      "choices": {
-        "D": "Deep Hang<br>",
-        "S": "Shallow Hang<br>",
-        "P": "Park<br>",
-        "F": "Failed Climb<br>",
-        "X": "Not attempted"
-      },
-      "defaultValue": "X"
-    }
-  ],
-  "postmatch": [
-    { "name": "Attained Coopertition Pt",
-      "code": "cop",
-      "type": "bool"
-    },
-       "defaultValue":"3"
-    },
-    { "name": "Died/Immobilized",
-      "code": "die",
-      "type": "bool"
-    },
-    { "name": "Tippy<br>(almost tipped over)",
-      "code": "tip",
-      "type": "bool"
-    },
-    { "name": "Comments",
-      "code": "co",
-      "type": "text",
-      "size": 15,
-      "maxSize": 55
-    }
-  ]
-}`;
+    "dataFormat": "tsv",
+    "title": "Reefscape Scouting 2025",
+    "page_title": "REEFSCAPE",
+    "checkboxAs": "10",
+    "prematch": [
+        {
+            "name": "Scouter",
+            "code": "s",
+            "type": "scouter",
+            "size": 5,
+            "maxSize": 99,
+            "required": "true"
+        },
+        {
+            "name": "Event",
+            "code": "e",
+            "type": "event",
+            "defaultValue": "2025milac",
+            "required": "true"
+        },
+        {
+            "name": "Match #",
+            "code": "m",
+            "type": "match",
+            "min": 1,
+            "max": 150,
+            "required": "true"
+        },
+        {
+            "name": "Robot",
+            "code": "r",
+            "type": "robot",
+            "choices": {
+                "r1": "Red-1",
+                "b1": "Blue-1<br>",
+                "r2": "Red-2",
+                "b2": "Blue-2<br>",
+                "r3": "Red-3",
+                "b3": "Blue-3"
+            },
+            "required": "true"
+        },
+        {
+            "name": "Team #",
+            "code": "t",
+            "type": "team",
+            "min": 1,
+            "max": 99999
+        }
+    ],
+    "auton": [
+        {
+            "name": "Leave Starting Line",
+            "code": "al",
+            "type": "bool"
+        },
+        {
+            "name": "Coral L1",
+            "code": "ac1",
+            "type": "counter"
+        },
+        {
+            "name": "Coral L2",
+            "code": "ac2",
+            "type": "counter"
+        },
+        {
+            "name": "Coral L3",
+            "code": "ac3",
+            "type": "counter"
+        },
+        {
+            "name": "Coral L4",
+            "code": "ac4",
+            "type": "counter"
+        },
+        {
+            "name": "Processor Score",
+            "code": "aps",
+            "type": "counter"
+        },
+        {
+            "name": "Net Score",
+            "code": "ans",
+            "type": "counter"
+        }
+    ],
+    "teleop": [
+        {
+            "name": "Coral L1",
+            "code": "tc1",
+            "type": "counter"
+        },
+        {
+            "name": "Coral L2",
+            "code": "tc2",
+            "type": "counter"
+        },
+        {
+            "name": "Coral L3",
+            "code": "tc3",
+            "type": "counter"
+        },
+        {
+            "name": "Coral L4",
+            "code": "tc4",
+            "type": "counter"
+        },
+        {
+            "name": "Processor Score",
+            "code": "tps",
+            "type": "counter"
+        },
+        {
+            "name": "Net Score",
+            "code": "tns",
+            "type": "counter"
+        },
+        {
+            "name": "Picked Up Coral From",
+            "code": "tpu",
+            "type": "radio",
+            "choices": {
+                "s": "Coral Station<br>",
+                "f": "Floor<br>",
+                "b": "Both<br>",
+                "x": "Not Attempted"
+            },
+            "defaultValue": "x"
+        },
+        {
+            "name": "Scored in Opponent Processor",
+            "code": "opp",
+            "type": "bool"
+        }
+    ],
+    "endgame": [
+        {
+            "name": "Barge Timer",
+            "code": "ebt",
+            "type": "timer"
+        },
+        {
+            "name": "Final Status",
+            "code": "efs",
+            "type": "radio",
+            "choices": {
+                "D": "Deep Hang<br>",
+                "S": "Shallow Hang<br>",
+                "P": "Park<br>",
+                "F": "Failed Climb<br>",
+                "X": "Not attempted"
+            },
+            "defaultValue": "X"
+        }
+    ],
+    "postmatch": [
+        {
+            "name": "Attained Coopertition Pt",
+            "code": "cop",
+            "type": "bool"
+        },
+        {
+            "name": "Died/Immobilized",
+            "code": "die",
+            "type": "bool"
+        },
+        {
+            "name": "Tippy<br>(almost tipped over)",
+            "code": "tip",
+            "type": "bool"
+        },
+        {
+            "name": "Comments",
+            "code": "co",
+            "type": "text",
+            "size": 15,
+            "maxSize": 55
+        }
+    ]
+}
