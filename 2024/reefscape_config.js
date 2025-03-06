a661d2f223730bda628f6c554a12b09200b5173a
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2025-03-06 09:46:08 -0500

    Update reefscape_config.js

--- 2024/reefscape_config.js
+++ 2024/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025milac",
+      "defaultValue": "2025mibig",
       "required": "true"
     },
     { "name": "Match Level",
